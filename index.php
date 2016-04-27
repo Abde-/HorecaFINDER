7db2c697772ce9efec6c3b07b40cef726856e504
Abdeselam El-Haman <aelhaman@gmail.com>
2016-04-28 01:08:05 +0200

    [Initial commit] First page + bootstrap

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>HorecaFINDER</title>
+		<meta charset="utf-8" />
+	</head>
+
+	<!-- Bootstrap CSS core -->
+	<link href="./bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<body>
+
+		<?php
+			// ce code va include les menus etc -> à mettre dans chaque page
+			include("include/entete.php");
+			include("include/menus.php");
+		?>
+
+		<?php
+			echo 'hello hello hello'; 
+		?>
+	</body>
+</html>
